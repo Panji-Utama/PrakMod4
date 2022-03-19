ecde12619bd6840a0c29bc93f7553807530290cf
FIRMAN PANJI <fpu2468@gmail.com>
2022-03-19 11:08:05 +0700

    menambahkan class SimpleDataBase

--- modul4_1302200089/modul4_1302200089/Program.cs
+++ modul4_1302200089/modul4_1302200089/Program.cs
@@ -10,6 +10,41 @@ namespace modul4_1302200089
     {
         static void Main(string[] args)
         {
+            SimpleDataBase<int> simpleData = new SimpleDataBase<int>();
+            simpleData.AddNewData(13);
+            simpleData.AddNewData(02);
+            simpleData.AddNewData(20);
+
+
+            simpleData.PrintData();
         }
     }
+
+    class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates; 
+        public SimpleDataBase()
+        {
+            this.storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T x)
+        {
+            storedData.Add(x);
+            inputDates.Add(DateTime.UtcNow);
+        }
+
+        public void PrintData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine("Data " + i + " berisi : " + storedData[i] + ", yang disimpan pada waktu UTC: " + inputDates[i]);
+            }
+
+        }
+    }
+
+
 }
